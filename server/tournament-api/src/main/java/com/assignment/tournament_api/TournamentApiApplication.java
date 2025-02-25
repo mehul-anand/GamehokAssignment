04ec4c001a098b245451e00bb391fe8c8948d0f0
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-02-26 02:48:56 +0530

    TournamentApi setup

---
+++ server/tournament-api/src/main/java/com/assignment/tournament_api/TournamentApiApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.tournament_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentApiApplication.class, args);
+	}
+
+}
---
+++ server/tournament-api/src/test/java/com/assignment/tournament_api/TournamentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.tournament_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
