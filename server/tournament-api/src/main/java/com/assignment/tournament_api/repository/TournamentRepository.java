5fdf53140e0860feaf40304418d2d7102b08edda
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-02-27 04:09:54 +0530

    tournament repo

--- server/tournament-api/src/main/java/com/assignment/tournament_api/repository/TournamentRepository.java
+++ server/tournament-api/src/main/java/com/assignment/tournament_api/repository/TournamentRepository.java
@@ -1,7 +1,7 @@
 package com.assignment.tournament_api.repository;
-
-import com.assignment.tournament_api.model.TournamentModel;
+import com.assignment.tournament_api.model.Tournament;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TournamentRepository extends JpaRepository<TournamentModel, Long> {
+public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+    // Additional query methods (if needed) can be defined here
 }
