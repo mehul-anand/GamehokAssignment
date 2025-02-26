1d0f59f720c1a535923f36432cb5afd71e0f709d
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-02-26 20:16:08 +0530

    landing page gradient and spelling correction

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,11 +2,11 @@ export default function Home() {
   return (
     <div className="relative container mx-auto px-4 pt-16 pb-16">
       <div className="max-w-5xl mx-auto text-center space-y-8">
-        <h1 className="text-5xl md:text-7xl lg:text-8xl mb-6">
-          GameHok Tournament
+        <h1 className="text-5xl md:text-7xl lg:text-8xl mb-6  gradient-text">
+          Gamehok Tournament
         </h1>
-        <p className="text-lg md:text-xl mb-8 text-gray-700">
-          Your onestop place for tournament uodates
+        <p className="text-lg md:text-xl mb-8 text-gray-700 gradient-text">
+          Your onestop place for tournament updates
         </p>
       </div>
     </div>
