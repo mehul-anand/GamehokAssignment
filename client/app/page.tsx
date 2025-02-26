16ea3e56a809e04ab1b746ac78eefa4ff2715473
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-02-26 20:12:44 +0530

    basic landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,14 @@
 export default function Home() {
   return (
-    <div>Testing Next App for GameHok</div>
+    <div className="relative container mx-auto px-4 pt-16 pb-16">
+      <div className="max-w-5xl mx-auto text-center space-y-8">
+        <h1 className="text-5xl md:text-7xl lg:text-8xl mb-6">
+          GameHok Tournament
+        </h1>
+        <p className="text-lg md:text-xl mb-8 text-gray-700">
+          Your onestop place for tournament uodates
+        </p>
+      </div>
+    </div>
   );
 }
