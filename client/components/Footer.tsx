bdbde2327331ac25850b932d3105b155f1cb0b4c
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-03-01 00:43:28 +0530

    update

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -6,7 +6,8 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-6">
         <p className="text-md  gradient-text">Gamehok Tournament</p>
         <div className="flex gap-4">
-          <p className="text-md">Made by Mehul Anand : </p>
+          <p className="text-md">Made by Mehul Anand</p>
+          <a href="https://mehul.xyz/" target="_blank" rel="noopener noreferrer">portfolio</a>
           {/* <a href="https://github.com/mehul-anand/GamehokAssignment" target="_blank" rel="noopener noreferrer">Repo</a> */}
         </div>
         <div className="flex space-x-4 mt-4 md:mt-0">
